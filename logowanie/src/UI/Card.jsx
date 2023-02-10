18e8bba1c571b4e03be5f19c98efd25fd350c3d0
DanielGarbuszewskiTIE9 <daniel.garbuszewski@cyfroweszkoly.pl>
2023-02-10 15:19:53 +0100

    3

--- logowanie/src/UI/Card.jsx
+++ logowanie/src/UI/Card.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import classes from './Card.module.css';
 
 const Card =(props)=>{
     return(
-        <div>
+        <div className={`${props.className} ${classes.card}`}>
             {props.children}
         </div>
     )
 }
+
+export default Card
