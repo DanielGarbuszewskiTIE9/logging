0935be138eb8f187538fc95999281147bf64254b
DanielGarbuszewskiTIE9 <daniel.garbuszewski@cyfroweszkoly.pl>
2023-02-16 15:19:30 +0100

    5

--- logowanie/src/components/AddUser.jsx
+++ logowanie/src/components/AddUser.jsx
@@ -1,19 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../UI/Card";
 import classes from './AddUser.module.css'
+import Button from "../UI/Button";
+
 
 function AddUser(){
+
+    const [name, setName] = useState('')
+    const [age, setAge] = useState('')
+
+    function namedChangeHandler(event){
+        setName(event.target.value)
+        console.log(name)
+    }
+
+    function agedChangeHandler(event){
+        setAge(event.target.value)
+        console.log(age)
+    }
+
+    function addUserHandler(event){
+        event.preventDefault()
+        console.log(name,age)
+        setName('')
+        setAge('')
+    }
+
     return(
         <Card className={classes.input}>
-        <form>
+        <form onSubmit={addUserHandler}>
             
             <label htmlFor="username">Username</label>
-            <input id="username"/>
+            <input id="username" onChange={namedChangeHandler}
+            value={name}/>
 
             <label htmlFor="age">Age</label>
-            <input id="age"/>
+            <input id="age" onChange={agedChangeHandler}
+            value={age}/>
 
-            <button type="submit">Add user</button>
+            <Button type="submit">Add user</Button>
         </form>
         </Card>
     )
