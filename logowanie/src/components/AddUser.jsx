9b2a55497480ed93cb2834102675d51b42fab139
DanielGarbuszewskiTIE9 <daniel.garbuszewski@cyfroweszkoly.pl>
2023-02-17 15:16:31 +0100

    afda

--- logowanie/src/components/AddUser.jsx
+++ logowanie/src/components/AddUser.jsx
@@ -2,12 +2,13 @@ import React, { useState } from "react";
 import Card from "../UI/Card";
 import classes from './AddUser.module.css'
 import Button from "../UI/Button";
-
+import ErrorModal from "../UI/ErrorModal";
 
 function AddUser(){
 
     const [name, setName] = useState('')
     const [age, setAge] = useState('')
+    const [errorModal, setErrorModal] = useState(null)
 
     function namedChangeHandler(event){
         setName(event.target.value)
@@ -21,12 +22,33 @@ function AddUser(){
 
     function addUserHandler(event){
         event.preventDefault()
-        console.log(name,age)
+        if(+age<1)
+        {
+            setErrorModal(
+                {
+                    title:'Błędny wiek',
+                    msg:"Wiek większy od zera"
+                }
+            )
+        }
+        if(name=='')
+        {
+            setErrorModal(
+                {
+                    title:'Błędna nazwa',
+                    msg:"Nazwa nie może być pusta"
+                }
+            )
+        }
+
         setName('')
+
         setAge('')
     }
 
     return(
+        <>
+        {errorModal && <ErrorModal title={errorModal.title} msg={errorModal.msg}/>}
         <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
             
@@ -41,7 +63,9 @@ function AddUser(){
             <Button type="submit">Add user</Button>
         </form>
         </Card>
+        </>
     )
+    
 }
 
 export default AddUser
